87fb1eae4cf49b23a63b57adee872012dafe7fec
Biken10 <bickey.crestha100@gmail.com>
2023-06-02 17:40:15 +0545

    codes

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+console.log("hello world")
+
+
+
+// variables/container/Box
+// -word whichcan store certian value
+
+
+greetings="Namaste"
+name="ram"
+age=20
+
+console.log(greetings,name)
+console.log(name,age)
+
