0e5f1e5d159053e0be0c06bcffb75f3764cd1926
Biken10 <bickey.crestha100@gmail.com>
2023-06-03 07:50:58 +0545

    codes

--- js/index.js
+++ js/index.js
@@ -6,6 +6,9 @@ console.log("hello world")
 // variables/container/Box
 // -word whichcan store certian value
 
+// javascript syntax 
+// name ="ram"
+
 
 greetings="Namaste"
 name="ram"
@@ -14,3 +17,49 @@ age=20
 console.log(greetings,name)
 console.log(name,age)
 
+
+// reserved keywords
+    // cannot create variables via them
+
+
+    course_name="mern"  //snake_case
+courseDuration=3  //camelCase
+
+number1="One"
+console.log(number1)
+
+// Data types 
+// 1.String -text 
+// 2.Number 
+//     -integer 
+//     -double/float/decimal
+// 3.Boolean
+//      -true 
+//      -false      
+
+/*to declare/create variable, use keyword first 
+     -var  (donot use it)
+     -let  (most prefered)
+     -const  
+ */
+  
+var laptopbrand="dell"
+console.log("laptop brand is ",laptopbrand);
+  
+let laptopcolor="white"
+  console.log("laptop color is",laptopcolor)
+
+const PI = 3.14
+console.log("the value of pie is",PI);  
+  
+
+// decalration vs initialization
+var mobileBrand //declaration
+mobileBrand = "samsung"  //initialization
+mobileBrand="apple"  //re-initialization
+console.log(mobileBrand)
+
+let watchBrand  //declaration
+watchBrand="swiss"  //initialization
+watchBrand="casio"  //re-initialization
+console.log(watchBrand)
