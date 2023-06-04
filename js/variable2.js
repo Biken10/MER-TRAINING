156a92a7c47708ef73fac71fc5d61dc0e2ff3bfb
Biken10 <bickey.crestha100@gmail.com>
2023-06-04 18:12:14 +0545

    codes

---
+++ js/variable2.js
@@ -0,0 +1,72 @@
+// Data types 
+// 1.String -text 
+// 2.Number 
+//     -integer 
+//     -double/float/decimal
+// 3.Boolean
+//      -true 
+//      -false 
+// undefined// in javascript only 
+// only 
+//  null 
+// non - primitive(colections /
+
+
+    // let brand = "apple"
+
+let color;
+color = "white"
+
+
+// console.log(brand)
+console.log(color)
+
+let isPaid = true;
+let isMarried = false;
+
+const PI = 3.14;
+console.log("the value of PIE is ", PI);
+
+//var vs let vs count 
+
+var name = "ram"
+var name = "ram"
+
+let surname = "xyz"
+// let surname ="xyz" cannot redeclare with let keyword
+surnmae = "abc"
+
+console.log(name)
+console.log(surname)
+
+let watchPrice
+console.log("beforeAssigning", watchPrice)
+
+watchPrice = 1000;
+
+
+
+
+let apiData = null //when we have to defin things as empty explicity
+
+let name1 = "ABC"
+let name2 = "DEF"
+let name3 = "XYZ"
+
+
+// array -collection of different values  
+//  let <variable_name>
+// let objectName=[array]
+let names = ["ABC", "DEF", "XYZ"]
+let brand = ["apple", "samsung"]
+console.log("names", names);
+console.log(brand);
+
+// order
+    // let objectName=// {<key>:value;  
+    // <property>:value;
+    // <attribute>:value;
+    // }
+let 
+
+
